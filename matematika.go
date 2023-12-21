f869488603c25acef0c8bce61c438d4b01518ac5
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-12-21 17:23:06 +0700

    belajar operasi matematika
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ matematika.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 10
+	var c = a + b
+	fmt.Println(c)
+
+	var d = 10
+	var e = 10
+	var f = d - e
+	fmt.Println(f)
+
+	var g = 10
+	var h = 10
+	var i = g * h
+	fmt.Println(i)
+
+	var j = 10
+	j += 10
+	fmt.Println(j)
+
+}
