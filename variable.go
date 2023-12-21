b2f8b0e90804d3029bd96f95d6483273088bfff1
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-12-21 16:37:53 +0700

    belajar variable
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string
+	//var name = "Daud Hidayat Ramadhan"
+	name := "Daud Hidayat Ramadhan"
+	fmt.Println(name)
+
+	name = "Bintang rahmatullah"
+	fmt.Println(name)
+
+	var (
+		firstName  = "Daud"
+		middleName = "Hidayat"
+		lastName   = "Ramadhan"
+	)
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
+}
