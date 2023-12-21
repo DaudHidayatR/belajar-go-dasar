ed4dff6663a7373f35445b5c86558d4dd12823d6
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-12-21 14:31:27 +0700

    belajar tipe data string
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Daud")
+	fmt.Println("Daud Hidayat")
+	fmt.Println("Daud Hidayat Ramadhan")
+
+	fmt.Println(len("Daud"))
+	fmt.Println("Daud Hidayat"[0])
+	fmt.Println("Daud Hidayat Ramadhan"[2])
+}
