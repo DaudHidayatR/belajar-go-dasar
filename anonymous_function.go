ee6e99e5e1f3935c6d7121f34817782923c571b9
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2024-12-31 19:22:13 +0700

    feat: anonymous_function
    Signed-off-by: Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>

---
+++ anonymous_function.go
@@ -0,0 +1,18 @@
+package main
+
+type Blacklist func(string) bool
+
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		println("You are blocked", name)
+	} else {
+		println("Welcome", name)
+	}
+}
+func main() {
+	blacklist := func(name string) bool {
+		return name == "anjing"
+	}
+	registerUser("daud", blacklist)
+	registerUser("anjing", blacklist)
+}
