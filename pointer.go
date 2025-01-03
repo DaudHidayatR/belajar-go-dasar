4ad80ee784570d9ee2ab73b9a2d670fd33c5d869
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2025-01-03 13:26:55 +0700

    feat: pointer
    Signed-off-by: Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>

---
+++ pointer.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	//Address1 := Address{"Jakarta", "DKI Jakarta", "Indonesia"}
+	//Address2 := Address1
+	//Address2.City = "Bandung"
+	//fmt.Println(Address1)
+	//fmt.Println(Address2)
+
+	Address1 := Address{"Jakarta", "DKI Jakarta", "Indonesia"}
+	Address2 := &Address1
+	Address2.City = "Bandung"
+	fmt.Println(Address1)
+	fmt.Println(Address2)
+
+}
